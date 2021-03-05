074dc2ff387a9b5855191aa961dc12a9077026a0
Matt <mattfsewell@gmail.com>
2021-03-05 11:29:17 -0500

    commit tree 786c4292e933d19314e8fa9423249e776f5dc9fb

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+function superbowlWin(array) {
+    let ret = array.find((obj) => {
+        return obj.result == "W"
+    })
+    if (ret){
+        return ret.year;
+    }
+    else {return undefined}
+}
